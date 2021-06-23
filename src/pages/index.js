4db739a5c6decc152ae7fcc447869d943da2af64
Tiago Ramos Alves <tiagoramosalves@hotmail.com>
2021-06-23 07:41:29 -0300

    feat: PostItem Estruturando os dados do PostItem

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,20 @@ import * as React from "react"
 
 import Layout from "../components/Layout"
 import Seo from "../components/seo"
+import PostItem from '../components/PostItem'
 
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>Home</h1>
-    
+    <PostItem 
+    slug="/about/"
+    background="blue"
+    category="Misc"
+    date="21 de Julho de 2021"
+    timeToRead="4"
+    title="Diga não ao Medium: tenha sua própria plataforma"
+    description="Algumas razões para você ter sua própria plataforma ao invés de soluções como o Medium."
+    />    
   </Layout>
 )
 
