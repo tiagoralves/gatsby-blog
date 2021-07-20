d88503811ffca180000e3558cf9f61c614437fd2
Tiago Ramos Alves <tiagoramosalves@hotmail.com>
2021-07-20 06:03:43 -0300

    feat: page blog-post

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,26 +1,28 @@
-import React from 'react'
-import {graphql} from 'gatsby'
+import React from "react"
+import { graphql } from "gatsby"
+
+import Layout from "../components/Layout"
 
 const BlogPost = ({ data }) => {
   const post = data.markdownRemark
 
   return (
-    <>
-    <h1>Title: {post.frontmatter.title}</h1>
-    <div dangerouslySetInnerHTML={{ __html: post.html }}></div>
-    </>
+    <Layout>
+      Title: {post.frontmatter.title}
+      <div dangerouslySetInnerHTML={{ __html: post.html }}></div>
+    </Layout>
   )
 }
 
 export const query = graphql`
-query Post($slug: String!) {
-  markdownRemark(fields: {slug: {eq: $slug}}) {
-    frontmatter {
-      title
+  query Post($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      frontmatter {
+        title
+      }
+      html
     }
-    html
   }
-}
 `
 
 export default BlogPost
